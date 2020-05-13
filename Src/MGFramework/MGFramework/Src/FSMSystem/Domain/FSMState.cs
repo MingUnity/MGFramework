6163d8bad1cc98ea0b478df76499462f60230b83
Ming <491913833@qq.com>
2020-05-13 21:42:38 +0800

    <Ming> 1.Add 增加 fsmsystem

---
+++ Src/MGFramework/MGFramework/Src/FSMSystem/Domain/FSMState.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+
+namespace MGFramework.FSM
+{
+    /// <summary>
+    /// FSM状态
+    /// </summary>
+    public abstract class FSMState : IFSMState
+    {
+        private Dictionary<int, IFSMState> _transitionDic = new Dictionary<int, IFSMState>();
+
+        /// <summary>
+        /// 获取目标状态
+        /// </summary>
+        public IFSMState this[int transition]
+        {
+            get
+            {
+                IFSMState res = null;
+
+                _transitionDic?.TryGetValue(transition, out res);
+
+                return res;
+            }
+            set
+            {
+                if (_transitionDic != null)
+                {
+                    _transitionDic[transition] = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 进入
+        /// </summary>
+        public abstract void OnEnter(params object[] keys);
+
+        /// <summary>
+        /// 退出
+        /// </summary>
+        public abstract void OnExit();
+
+        /// <summary>
+        /// 驻留
+        /// </summary>
+        public abstract void OnStay();
+    }
+}
