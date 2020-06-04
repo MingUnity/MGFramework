6a253d535379a3b88bb0971a80e93d87076f6d7b
Ming <centmeter@outlook.com>
2020-06-04 13:17:58 +0800

    <Ming> 1.Add 增加recttransform 包含判定逻辑

--- Src/MGFramework/MGFramework/Src/UIModule/Extends/TransformExtends.cs
+++ Src/MGFramework/MGFramework/Src/UIModule/Extends/TransformExtends.cs
@@ -20,5 +20,82 @@ namespace MGFramework
 
             return t;
         }
+
+        /// <summary>
+        /// 包含某元素
+        /// 只要有一个角在容器内即返回true
+        /// </summary>
+        /// <param name="container">容器</param>
+        /// <param name="trans">检测对象</param>
+        public static bool Contains(this RectTransform container, RectTransform trans)
+        {
+            if (trans == null)
+            {
+                return false;
+            }
+
+            Vector3[] containerCorners = new Vector3[4];
+            container.GetWorldCorners(containerCorners);
+            float width = Mathf.Abs(containerCorners[2].x - containerCorners[0].x);
+            float height = Mathf.Abs(containerCorners[2].y - containerCorners[0].y);
+            Rect rect = new Rect(containerCorners[0].x, containerCorners[0].y, width, height);
+
+            Vector3[] rtCorners = new Vector3[4];
+            trans.GetWorldCorners(rtCorners);
+
+            bool res = false;
+
+            for (int i = 0; i < rtCorners.Length; i++)
+            {
+                Vector3 corner = rtCorners[i];
+
+                if (rect.Contains(corner))
+                {
+                    res = true;
+                    break;
+                }
+            }
+
+            return res;
+        }
+
+        /// <summary>
+        /// 全部包含某元素
+        /// 被检测对象全部包含在容器中
+        /// </summary>
+        /// <param name="container">容器</param>
+        /// <param name="trans">检测对象</param>
+        public static bool TotalContains(this RectTransform container, RectTransform trans)
+        {
+            if (trans == null)
+            {
+                return false;
+            }
+
+            Vector3[] containerCorners = new Vector3[4];
+            container.GetWorldCorners(containerCorners);
+            float width = Mathf.Abs(containerCorners[2].x - containerCorners[0].x);
+            float height = Mathf.Abs(containerCorners[2].y - containerCorners[0].y);
+            Rect rect = new Rect(containerCorners[0].x, containerCorners[0].y, width, height);
+
+            Vector3[] rtCorners = new Vector3[4];
+            trans.GetWorldCorners(rtCorners);
+
+            bool res = true;
+
+            for (int i = 0; i < rtCorners.Length; i++)
+            {
+                Vector3 corner = rtCorners[i];
+
+                if (!rect.Contains(corner))
+                {
+                    res = false;
+                    break;
+                }
+            }
+
+            return res;
+        }
+
     }
 }
