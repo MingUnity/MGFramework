65df4bde9b9559b5bbf19b0e286ffd83232d42d7
Ming <centmeter@outlook.com>
2020-06-30 15:55:33 +0800

    <Ming> 1.Modify 重构SuperScrollView

---
+++ Src/MGFramework/MGFramework/Src/UIModule/Components/SuperScrollView/ISuperScrollNode.cs
@@ -0,0 +1,13 @@
+﻿namespace MGFramework.UIModule
+{
+    /// <summary>
+    /// 滚动节点
+    /// </summary>
+    public interface ISuperScrollNode
+    {
+        /// <summary>
+        /// 更新数据
+        /// </summary>
+        void Refresh(ISuperScrollNodeData data);
+    }
+}
---
+++ Src/MGFramework/MGFramework/Src/UIModule/Components/SuperScrollView/ISuperScrollNodeData.cs
@@ -0,0 +1,10 @@
+﻿namespace MGFramework.UIModule
+{
+    /// <summary>
+    /// 滚动列表节点数据
+    /// </summary>
+    public interface ISuperScrollNodeData
+    {
+
+    }
+}
