92ba12f25df21252aab97240f781572de095fee5
Ming <491913833@qq.com>
2020-07-04 15:04:19 +0800

    <Ming> 1.Add 增加参数系统

---
+++ Src/MGFramework/MGFramework/Src/ArgSystem/ArgSystem.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MGFramework.Args
+{
+    /// <summary>
+    /// 参数存取系统
+    /// </summary>
+    public static class ArgSystem<T> where T : struct
+    {
+        /// <summary>
+        /// 存储池
+        /// </summary>
+        private static Dictionary<int, Func<T>> _pool = new Dictionary<int, Func<T>>();
+
+        /// <summary>
+        /// 获取数据
+        /// </summary>
+        public static bool Get(int argId, out T arg)
+        {
+            bool result = false;
+
+            Func<T> func = _pool.GetValueAnyway(argId);
+
+            if (func != null)
+            {
+                arg = func.Invoke();
+                result = true;
+            }
+            else
+            {
+                arg = default(T);
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// 预存数据
+        /// </summary>
+        public static void Set(int argId, Func<T> argFunc)
+        {
+            _pool[argId] = argFunc;
+        }
+
+        /// <summary>
+        /// 移除
+        /// </summary>
+        public static void Remove(int argId)
+        {
+            _pool.Remove(argId);
+        }
+
+        /// <summary>
+        /// 清除所有
+        /// </summary>
+        public static void Clear()
+        {
+            _pool.Clear();
+        }
+    }
+}
---
+++ Src/MGFramework/MGFramework/Src/UIModule/Components/SliderEx.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine.UI;
+
+namespace MGFramework.UIModule
+{
+    /// <summary>
+    /// Slider扩展
+    /// </summary>
+    public class SliderEx : Slider
+    {
+        /// <summary>
+        /// 设置值 且不抛事件
+        /// </summary>
+        public void SetValueWithoutNotify(float value)
+        {
+            Set(value, false);
+        }
+    }
+}
