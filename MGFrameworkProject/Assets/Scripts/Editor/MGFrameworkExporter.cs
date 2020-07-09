1715ae34dd4a907f077a62cb71bc0592bfeca4df
Ming <centmeter@outlook.com>
2020-07-09 09:55:13 +0800

    <Ming> 1.Add 增加自动化导包工具 2.Add 增加图片键值存储脚本

---
+++ MGFrameworkProject/Assets/Scripts/Editor/MGFrameworkExporter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class MGFrameworkExporter
+{
+    [MenuItem("Tools/ExportMGFramework #E")]
+    public static void Export()
+    {
+        string assetPath = "Assets/MGFramework";
+        string packageName = "MGFramework.unitypackage";
+        string outputDir = Path.Combine(Application.dataPath, "../../Output");
+        if (!Directory.Exists(outputDir))
+        {
+            Directory.CreateDirectory(outputDir);
+        }
+        AssetDatabase.ExportPackage(assetPath, Path.Combine(outputDir, packageName), ExportPackageOptions.Recurse);
+        Application.OpenURL(outputDir);
+    }
+}
