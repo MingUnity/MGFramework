c2ce900ce497312fe037ac720775e1ab3386d365
Ming <491913833@qq.com>
2020-07-21 23:23:51 +0800

    <Ming> 1.Add 新增图片缓存加载工具

---
+++ Src/MGFramework/MGFramework/Src/Utility/MD5.cs
@@ -0,0 +1,33 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace MGFramework
+{
+    /// <summary>
+    /// MD5码工具
+    /// </summary>
+    public static class MD5
+    {
+        /// <summary>
+        /// 获取
+        /// </summary>
+        public static string Get(string data)
+        {
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+
+            byte[] byteValue = Encoding.UTF8.GetBytes(data);
+            byte[] byteHash = md5.ComputeHash(byteValue);
+
+            md5.Clear();
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            for (int i = 0; i < byteHash.Length; i++)
+            {
+                stringBuilder.Append(byteHash[i].ToString("X").PadLeft(2, '0'));
+            }
+
+            return stringBuilder.ToString().ToLower();
+        }
+    }
+}
---
+++ Src/MGFramework/MGFramework/Src/Utility/TextureLoader/TextureLoader.cs
@@ -0,0 +1,296 @@
+﻿using MGFramework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace MGFramework
+{
+    /// <summary>
+    /// 图片加载
+    /// </summary>
+    public static class TextureLoader
+    {
+        /// <summary>
+        /// 图片内存池
+        /// </summary>
+        private static MemoryTexturePool _memPool;
+
+        /// <summary>
+        /// 图片本地池
+        /// </summary>
+        private static LocalDiskTexturePool _diskPool;
+
+        /// <summary>
+        /// 网络图片加载
+        /// </summary>
+        private static HttpTextureLoader _httpLoader;
+
+        /// <summary>
+        /// 异步任务元素集合
+        /// </summary>
+        private static Queue<AsyncItem> _asyncTaskItems = new Queue<AsyncItem>();
+
+        /// <summary>
+        /// 异步任务
+        /// </summary>
+        private static Task _asyncTask;
+
+        static TextureLoader()
+        {
+            _memPool = new MemoryTexturePool();
+            _diskPool = new LocalDiskTexturePool();
+            _httpLoader = new HttpTextureLoader();
+        }
+
+        /// <summary>
+        /// 异步加载
+        /// 无缓存机制
+        /// </summary>
+        public static void Load(string url, Action<Texture2D> callback)
+        {
+            if (string.IsNullOrEmpty(url))
+            {
+                callback?.Invoke(null);
+                return;
+            }
+
+            _httpLoader.Load(url, callback);
+        }
+
+        /// <summary>
+        /// 加载
+        /// 若已被缓存 则加载缓存
+        /// 若未被缓存 则走网络请求
+        /// </summary>
+        /// <param name="url">图片路径</param>
+        /// <param name="callback">完成回调</param>
+        /// <param name="cacheLevel">缓存优先级</param>
+        public static void Load(string url, Action<Texture2D> callback, CacheLevel cacheLevel)
+        {
+            if (string.IsNullOrEmpty(url))
+            {
+                callback?.Invoke(null);
+                return;
+            }
+
+            string key = ConvertKey(url);
+
+            Texture2D tex = null;
+
+            if (_memPool.Get(key, out tex))
+            {
+                callback?.Invoke(tex);
+            }
+            else if (_diskPool.Get(key, out tex))
+            {
+                callback?.Invoke(tex);
+                _memPool.Cache(key, tex, cacheLevel);
+            }
+            else
+            {
+                _httpLoader.Load(url, (resTex) =>
+                {
+                    callback?.Invoke(resTex);
+
+                    if (resTex != null)
+                    {
+                        _memPool.Cache(key, resTex, cacheLevel);
+                        _diskPool.Cache(key, resTex, cacheLevel);
+                    }
+                });
+            }
+        }
+
+        /// <summary>
+        /// 自定义加载图片
+        /// 走缓存池
+        /// </summary>
+        /// <param name="key">缓存键,若为空则取消加载</param>
+        /// <param name="customLoader">自定义加载器</param>
+        /// <param name="callback">图片加载完成回调</param>
+        /// <param name="cacheLevel">缓存优先级</param>
+        public static void CustomLoad(string key, ICustomTextureLoader customLoader, Action<Texture2D> callback, CacheLevel cacheLevel)
+        {
+            if (string.IsNullOrEmpty(key) || customLoader == null)
+            {
+                callback?.Invoke(null);
+                return;
+            }
+
+            key = ConvertKey(key);
+
+            Texture2D tex = null;
+
+            if (_memPool.Get(key, out tex))
+            {
+                callback?.Invoke(tex);
+            }
+            else if (_diskPool.Get(key, out tex))
+            {
+                callback?.Invoke(tex);
+                _memPool.Cache(key, tex, cacheLevel);
+            }
+            else
+            {
+                customLoader.Load((resTex) =>
+                {
+                    callback?.Invoke(resTex);
+
+                    if (resTex != null)
+                    {
+                        _memPool.Cache(key, resTex, cacheLevel);
+                        _diskPool.Cache(key, resTex, cacheLevel);
+                    }
+                });
+            }
+        }
+
+        /// <summary>
+        /// 预加载
+        /// 进缓存池
+        /// </summary>
+        /// <param name="url">图片路径</param>
+        /// <param name="cacheLevel">缓存优先级</param>
+        public static void Preload(string url, CacheLevel cacheLevel)
+        {
+            Load(url, null, cacheLevel);
+        }
+
+        /// <summary>
+        /// 批量预加载
+        /// 进缓存池
+        /// </summary>
+        /// <typeparam name="T">实体对象</typeparam>
+        /// <param name="array">对象集合</param>
+        /// <param name="urlGetter">获取url方法</param>
+        /// <param name="cacheLevel">缓存优先级</param>
+        public static void Preload<T>(T[] array, Func<T, string> urlGetter, CacheLevel cacheLevel)
+        {
+            if (array == null || urlGetter == null)
+            {
+                return;
+            }
+
+            string[] urls = new string[array.Length];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                urls[i] = urlGetter.Invoke(array[i]);
+            }
+
+            Task.CreateTask(PreloadPerFrame(urls, cacheLevel));
+        }
+
+        /// <summary>
+        /// 加入异步加载任务队列
+        /// 每帧加载一个资源
+        /// </summary>
+        public static void LoadByTask(string url, Action<Texture2D> callback, CacheLevel cacheLevel)
+        {
+            if (_asyncTask == null)
+            {
+                _asyncTask = Task.CreateTask(AsyncTask());
+            }
+
+            _asyncTaskItems.Enqueue(new AsyncItem()
+            {
+                url = url,
+                callback = callback,
+                cacheLevel = cacheLevel
+            });
+        }
+
+        /// <summary>
+        /// 设置本地缓存池最大缓存量
+        /// </summary>
+        public static void SetLocalDiskMaxCacheMemory(long maxMemory)
+        {
+            _diskPool.SetMaxCacheMemory(maxMemory);
+        }
+
+        /// <summary>
+        /// 设置本地缓存池缓存目录
+        /// </summary>
+        public static void SetLocalDiskCacheDir(string dir)
+        {
+            _diskPool.SetCacheDir(dir);
+        }
+
+        /// <summary>
+        /// 设置内存缓存池最大缓存量
+        /// </summary>
+        public static void SetMemoryMaxCacheMemory(long maxMemory)
+        {
+            _memPool.SetMaxCacheMemory(maxMemory);
+        }
+
+        /// <summary>
+        /// 异步任务
+        /// </summary>
+        private static IEnumerator AsyncTask()
+        {
+            int emptyTaskCount = 0;
+            int endCount = 10;
+
+            while (true)
+            {
+                if (_asyncTaskItems.Count > 0)
+                {
+                    emptyTaskCount = 0;
+                    AsyncItem item = _asyncTaskItems.Dequeue();
+
+                    if (item != null)
+                    {
+                        Load(item.url, item.callback, item.cacheLevel);
+                    }
+                }
+                else
+                {
+                    if (++emptyTaskCount >= endCount)
+                    {
+                        break;
+                    }
+                }
+
+                yield return null;
+            }
+
+            _asyncTask = null;
+        }
+
+        /// <summary>
+        /// 分帧预加载图片
+        /// </summary>
+        private static IEnumerator PreloadPerFrame(string[] urls, CacheLevel cacheLevel)
+        {
+            if (urls != null && urls.Length > 0)
+            {
+                for (int i = 0; i < urls.Length; i++)
+                {
+                    Load(urls[i], null, cacheLevel);
+                    yield return null;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 转化成key
+        /// </summary>
+        private static string ConvertKey(string url)
+        {
+            return MD5.Get($"{url}_{Application.version}");
+        }
+
+        /// <summary>
+        /// 异步元素
+        /// </summary>
+        private class AsyncItem
+        {
+            public string url;
+            public Action<Texture2D> callback;
+            public CacheLevel cacheLevel;
+        }
+    }
+}
