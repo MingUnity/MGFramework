18ea434423c6f2099d77f7d5284a2ad637b4fcac
Ming <centmeter@outlook.com>
2020-07-23 11:50:10 +0800

    <Ming> 1.Modify 补充注释

--- Src/MGFramework/MGFramework/Src/UIModule/Components/SuperScrollView/SuperScrollNodeBase.cs
+++ Src/MGFramework/MGFramework/Src/UIModule/Components/SuperScrollView/SuperScrollNodeBase.cs
@@ -4,15 +4,27 @@ namespace MGFramework.UIModule
 {
     public abstract class SuperScrollNodeBase : ISuperScrollNode
     {
+        /// <summary>
+        /// 根节点
+        /// </summary>
         protected abstract Transform Root { get; }
 
+        /// <summary>
+        /// 重置需要异步加载的数据
+        /// </summary>
         public abstract void ResetAsyncData();
 
+        /// <summary>
+        /// 移至首节点
+        /// </summary>
         public virtual void TurnFirst()
         {
             Root?.SetAsFirstSibling();
         }
 
+        /// <summary>
+        /// 移至尾节点
+        /// </summary>
         public virtual void TurnLast()
         {
             Root?.SetAsLastSibling();
