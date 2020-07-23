acf92b73488ad2b3dd75456b9a75d4f3f5a4a8f6
Ming <centmeter@outlook.com>
2020-07-23 15:24:31 +0800

    <Ming> 1.Modify 重构scrollnode

--- MGFrameworkProject/Assets/Scripts/SuperScrollSample/ScrollNode.cs
+++ MGFrameworkProject/Assets/Scripts/SuperScrollSample/ScrollNode.cs
@@ -2,34 +2,43 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ScrollNode : SuperScrollNodeBase
+public class ScrollNode : ISuperScrollNode
 {
     private RawImage _img;
+    
+    public string TexKey
+    {
+        set
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                _img.texture = null;
+                return;
+            }
+
+            ResourcesTextureLoader.Load(value, (tex) => _img.texture = tex);
+        }
+    }
+
+    public bool Active { get => _img.gameObject.activeSelf; set => _img.gameObject.SetActive(value); }
 
     public void Create(Transform root)
     {
         _img = root.GetComponent<RawImage>();
     }
 
-    public override void ResetAsyncData()
+    public void TurnLast()
+    {
+        _img.transform.SetAsLastSibling();
+    }
+
+    public void TurnFirst()
+    {
+        _img.transform.SetAsFirstSibling();
+    }
+
+    public void ResetAsyncData()
     {
         TexKey = null;
     }
-
-    public string TexKey
-    {
-        set
-        {
-            if(string.IsNullOrEmpty(value))
-            {
-                _img.texture = null;
-                return;
-            }
-            
-            ResourcesTextureLoader.Load(value, (tex) => _img.texture = tex);
-        }
-    }
-    public bool Active { get => _img.gameObject.activeSelf; set => _img.gameObject.SetActive(value); }
-
-    protected override Transform Root => _img.transform;
 }
