ffac3e561f63d4c3243dd266b948c3fdd41e6009
Ming <centmeter@outlook.com>
2020-09-04 13:47:32 +0800

    <Ming> 1.Add 增加n阶贝塞尔曲线计算

--- Src/MGFramework/MGFramework/Src/Utility/Bezier.cs
+++ Src/MGFramework/MGFramework/Src/Utility/Bezier.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace MGFramework
 {
@@ -22,6 +24,61 @@ namespace MGFramework
             p += tt * p2;
 
             return p;
+        }
+
+        /// <summary>
+        /// 绘制n阶贝塞尔曲线路径
+        /// </summary>
+        /// <param name="poss">控制点数组</param>
+        /// <param name="precision">输出数量</param>
+        public static Vector3[] Caculate(Vector3[] poss, int precision)
+        {
+            //贝塞尔曲线控制点数（阶数）
+            int number = poss.Length;
+
+            //控制点数不小于 2
+            if (number < 2)
+                return null;
+
+            //计算杨辉三角
+            int[] mi = new int[number];
+            mi[0] = mi[1] = 1;
+            for (int i = 3; i <= number; i++)
+            {
+                int[] t = new int[i - 1];
+                for (int j = 0; j < t.Length; j++)
+                {
+                    t[j] = mi[j];
+                }
+
+                mi[0] = mi[i - 1] = 1;
+                for (int j = 0; j < i - 2; j++)
+                {
+                    mi[j + 1] = t[j] + t[j + 1];
+                }
+            }
+
+            Vector3[] result = new Vector3[precision];
+
+            for (int i = 0; i < precision; i++)
+            {
+                float t = i * 1.0f / precision;
+                double x = 0;
+                double y = 0;
+                double z = 0;
+                for (int j = 0; j < number; j++)
+                {
+                    Vector3 p = poss[j];
+                    double scale = Math.Pow(1 - t, number - j - 1) * Math.Pow(t, j) * mi[j];
+
+                    x += scale * p.x;
+                    y += scale * p.y;
+                    z += scale * p.z;
+                }
+                result[i] = new Vector3((float)x, (float)y, (float)z);
+            }
+
+            return result;
         }
     }
 }
